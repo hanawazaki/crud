e4fd64df80ef689f3dc88e6290a9cd1a9f9bcba1
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-12-31 19:20:25 +0700

    add migration

--- database/migrations/2023_12_31_121352_create_members_table.php
+++ database/migrations/2023_12_31_121352_create_members_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('persons', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("email");
+            $table->string("phone");
+            $table->string("gender");
+            $table->string("status");
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('persons');
+        Schema::dropIfExists('members');
     }
 };
